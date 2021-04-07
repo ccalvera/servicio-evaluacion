4b4adb8725c35ae24f0c5f58691e8e0f8f123606
Diego Calvera <dfch100@gmail.com>
2021-04-06 19:27:15 -0500

    Creacion proyecto Servicio de Evaluacion

---
+++ src/main/java/co/edu/utp/isc/gia/servicioevaluacion/ServicioEvaluacionApplication.java
@@ -0,0 +1,13 @@
+package co.edu.utp.isc.gia.servicioevaluacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioEvaluacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioEvaluacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/utp/isc/gia/servicioevaluacion/ServicioEvaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.isc.gia.servicioevaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioEvaluacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
